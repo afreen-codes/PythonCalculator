4b5d53b8455f02d40a78f430607e0ee7d6055c64
afreen-codes <afreenansari6387@gmail.com>
2025-11-18 14:58:33 +0530

    Calculator program

---
+++ pythonCalculator.py
@@ -0,0 +1,40 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    return x / y
+def square(x, y):
+    return x** y
+  
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+print("5. Square")
+
+choice = input("Enter choice (1/2/3/4/5): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print(f"Result: {add(num1, num2)}")
+elif choice == '2':
+    print(f"Result: {subtract(num1, num2)}")
+elif choice == '3':
+    print(f"Result: {multiply(num1, num2)}")
+elif choice == '4':
+    print(f"Result: {divide(num1, num2)}")
+elif choice == '5':
+    print(f"Result: {square(num1, num2)}")
+else:
+    print("Invalid input!")
